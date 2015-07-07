d928f699b2a835aeaa607e7bfa5c880096893849
matank <matank@jfrog.com>
2015-07-07 12:17:40 +0300

    Change dockerExec to send list instead of string

--- src/main/groovy/org/jfrog/util/docker/DockerExec.groovy
+++ src/main/groovy/org/jfrog/util/docker/DockerExec.groovy
@@ -35,7 +35,7 @@ class DockerExec {
 
     DockerExec(DockerContainer dockerContainer, def commands) {
         this.dockerContainer = dockerContainer
-        this.commands = commands instanceof List ? commands : commands.split(" ")
+        this.commands = commands instanceof List ? commands : ["/bin/bash", "-c", commands]
         this.attachStdin = attachStdin
         this.attachStdout = attachStdout
         this.attachStderr = attachStderr
